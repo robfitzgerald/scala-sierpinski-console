7a7253397b7d4dc6600869299717ab28a4d45df8
Robert Fitzgerald <robfitzgerald@gmail.com>
2016-07-18 21:55:26 -0600

    added scaladoc annotations. Point.mark uses default value.

--- src/main/scala/Sierpinski.scala
+++ src/main/scala/Sierpinski.scala
@@ -1,41 +1,59 @@
-import scala.annotation.tailrec
-
 object Sierpinski extends App {
 
-	case class Point(x: Int ,y: Int, mark: String)
-	val UNSET = ""
+	/**
+	 * represents a point on the 2 dimensional coordinate plane
+	 * @type {Point}
+	 * @param {Int}      x:      x-coordinate
+	 * @param {Int}      y:      y-coordinate
+	 * @param {String}   mark:   image to use when printing this point to the screen. defaults to an unset value.
+	 */
+	case class Point(x: Int ,y: Int, mark: String = "")
   
-	def drawAtDepth(n: Int, height: Int) {
-		if (n < 0) throw new ArithmeticException()
-		if (height < 0) throw new ArithmeticException()
+	/**
+	 * head of the recursive tree that builds triangles, combines them with the background, and prints it to the console.
+	 * @param  {Int} 		depth:      depth to recurse
+	 * @param  {Int}    height: height of output, which also calculates the width.
+	 */
+	def drawAtDepth(depth: Int, height: Int) {
+		if (depth < 0 || height < 0) throw new ArithmeticException()
 		val yBounds = height
 		val xBounds = (height * 2) - 1	
 
-		val trianglePoints = drawTriangles(0, n, Point(0,0,""), Point(xBounds,yBounds,"")).distinct.toSeq
-		val background = drawBackground(xBounds,yBounds).diff(trianglePoints).map(p=>Point(p.x,p.y,"_"))
-		val triangles = trianglePoints.map(p=>Point(p.x,p.y,"1"))
-		val trianglesOnBackground = triangles.union(background).sortBy(p=>(0 - p.y,p.x))
+		val trianglePoints = drawTriangles(0, depth, Point(0,0), Point(xBounds,yBounds)).distinct.toSeq
+		val backgroundPointsMarked = drawBackground(xBounds,yBounds).diff(trianglePoints).map(p=>Point(p.x,p.y,"_"))
+		val trianglesMarked = trianglePoints.map(p=>Point(p.x,p.y,"1"))
+		val trianglesOnBackground = trianglesMarked.union(backgroundPointsMarked).sortBy(p=>(0 - p.y,p.x))
 		val image = trianglesOnBackground.map(paint=>paint.mark).grouped(xBounds)
 		image.foreach(line=>println(line.mkString))
 	}
 
+	/**
+	 * creates point objects to cover the 2-dimensional coordinate plane on integer steps
+	 * @param   {Int}   xBounds:     width of plane, from 0 to xBounds
+	 * @param   {Int}   yBounds:     height of plane, from 0 to yBounds
+	 * @return  {Seq[Point]}         all values that can exist in this image
+	 */
 	def drawBackground(xBounds: Int, yBounds: Int): Seq[Point] = {
-		(for (x <- 0 until xBounds; y <- 0 until yBounds) yield Point(x,y,"")).toSeq
+		(for (x <- 0 until xBounds; y <- 0 until yBounds) yield Point(x,y)).toSeq
 	}
 
-	/* 
+	/**
 	 * draws a triangle from origin to terminal, not-inclusive
+	 * @param {Int}	    depth:      current recursion depth
+	 * @param {Int}     maxDepth:   point at which to stop recursing and begin creating triangle points
+	 * @param {Point}   origin:     coordinate value for the bottom-left corner of this triangle section
+	 * @param {Point}   terminal:   coordinate that represents the x and y bounds of this triangle section
+	 * @return {Seq[Point]}         all points on triangles related to the triangle between origin and terminal
 	 */
-
 	def drawTriangles(depth: Int, maxDepth: Int, origin: Point, terminal: Point): Seq[Point] = {
   	if (depth == maxDepth) {
-			val xRelativeHalfwayPoint = ((terminal.x.toFloat - origin.x.toFloat) / 2.0).ceil.toInt
-			val yRelativeHeight = terminal.y - origin.y
+			val halfwayX = ((terminal.x.toFloat - origin.x.toFloat) / 2.0).ceil.toInt
+			val height = terminal.y - origin.y
 			(
-				(for (x <- 0 until xRelativeHalfwayPoint; y <- 0 until yRelativeHeight if (y <= x)) yield 
-					Point(x+origin.x, y + origin.y, "")) ++
-				(for (x <- 0 until (xRelativeHalfwayPoint - 1); y <- 0 until yRelativeHeight if (y < ((yRelativeHeight - 1) - x))) yield 
-					Point(x + origin.x + xRelativeHalfwayPoint, y + origin.y, "")))
+				(for (x <- 0 until halfwayX; y <- 0 until height if (y <= x)) yield 
+					Point(x+origin.x, y + origin.y)) ++
+				(for (x <- 0 until (halfwayX - 1); y <- 0 until height if (y < ((height - 1) - x))) yield 
+					Point(x + origin.x + halfwayX, y + origin.y)))
 		} else {
 			((drawTriangles(
 				depth + 1, 
@@ -55,54 +73,81 @@ object Sierpinski extends App {
 		}	
 	}	
 
+	/**
+	 * calcuates a terminal point for the bottom-left sub-triangle
+	 * @param {Point}   origin:    first coordinate of the sub-section
+	 * @param {Point}   terminal:  x and y bounds of the sub-section
+	 * @return {Point} terminal point of the sub-section
+	 */
 	def bottomLeftEndPoint(origin: Point, terminal: Point): Point = {
 		new Point(
 				(origin.x + ((terminal.x.toFloat - origin.x.toFloat) / 2.0).ceil.toInt),
-				(origin.y + ((terminal.y.toFloat - origin.y.toFloat) / 2.0).ceil.toInt),
-				UNSET
+				(origin.y + ((terminal.y.toFloat - origin.y.toFloat) / 2.0).ceil.toInt)
 			)
 	}
 
+	/**
+	 * calcuates an origin point for the bottom-right sub-triangle
+	 * @param {Point}   origin:    first coordinate of the sub-section
+	 * @param {Point}   terminal:  x and y bounds of the sub-section
+	 * @return {Point} origin point of the sub-section
+	 */
 	def bottomRightStartPoint(origin: Point, terminal: Point): Point = {
 		new Point(
 				(origin.x + ((terminal.x.toFloat - origin.x.toFloat) / 2.0).ceil.toInt),
-				(origin.y),
-				UNSET
+				(origin.y)
 			)
 	}
 
+	/**
+	 * calcuates a terminal point for the bottom-right sub-triangle
+	 * @param {Point}   origin:    first coordinate of the sub-section
+	 * @param {Point}   terminal:  x and y bounds of the sub-section
+	 * @return {Point} terminal point of the sub-section
+	 */
 	def bottomRightEndPoint(origin: Point, terminal: Point): Point = {
 		new Point(
 				(terminal.x),
-				(origin.y + ((terminal.y.toFloat - origin.y.toFloat) / 2.0).ceil.toInt),
-				UNSET
+				(origin.y + ((terminal.y.toFloat - origin.y.toFloat) / 2.0).ceil.toInt)
 			)
 	}
 
+	/**
+	 * calcuates an origin point for the top sub-triangle
+	 * @param {Point}   origin:    first coordinate of the sub-section
+	 * @param {Point}   terminal:  x and y bounds of the sub-section
+	 * @return {Point} origin point of the sub-section
+	 */
 	def topStartPoint(origin: Point, terminal: Point): Point = {
 		new Point(
 				(origin.x + ((terminal.x.toFloat - origin.x.toFloat) / 4.0).ceil.toInt),
-				(origin.y + ((terminal.y.toFloat - origin.y.toFloat) / 2.0).ceil.toInt),
-				UNSET
+				(origin.y + ((terminal.y.toFloat - origin.y.toFloat) / 2.0).ceil.toInt)
 			)
 	}
 
+	/**
+	 * calcuates a terminal point for the top sub-triangle
+	 * @param {Point}   origin:    first coordinate of the sub-section
+	 * @param {Point}   terminal:  x and y bounds of the sub-section
+	 * @return {Point} terminal point of the sub-section
+	 */
 	def topEndPoint(origin: Point, terminal: Point): Point = {
 		new Point(
 				(origin.x + (((terminal.x.toFloat - origin.x.toFloat) / 4.0).ceil.toInt) * 3),
-				(terminal.y),
-				UNSET
+				(terminal.y)
 			)
 	}
 
+	/**
+	 * main program
+	 */
 	try {
 		args.length match {
 			case 0 => drawAtDepth(1, 32)
 			case 1 => drawAtDepth(args(0).toInt, 32)
 			case 2 => drawAtDepth(args(0).toInt, args(1).toInt)
 		}
-    // drawAtDepth(readInt(), 32)
 	} catch {
-	  case e: Exception => println("usage: run <depth> <height> for e^<depth> triangles printed to <height> lines on the console.");
+	  case e: Exception => println("usage: run <depth> <height> for e^<depth> triangles printed to <height> lines on the console.   (" + e + ")");
 	}
 }
